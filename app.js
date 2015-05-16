d6b4361e4b86aab967a6a2652e596fc06d542141
Patrick Waivers <pwaivers@gmail.com>
2015-05-16 17:21:16 -0400

    Add resume downloading options

---
+++ app.js
@@ -0,0 +1,15 @@
+// this AngularJS stuff is incomplete
+
+(function(){
+    'use strict';
+
+    angular.module('app', []);
+
+    angular.module('app')
+        .controller('MainController', function(){
+            var that = this;
+
+            that.phone = '(252) 714-5764';
+        });
+
+})();
